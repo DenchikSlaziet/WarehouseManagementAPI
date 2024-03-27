43bc2c2337c60ed01c51c039708c409296054605
DenchikSlaziet <kochetkovdenic1@gmail.com>
2024-03-27 17:33:35 +0300

    Написал валидаторы для моделей и сервис валидации

---
+++ WarehouseManagement.Services/Validators/WarehouseModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using WarehouseManagement.Services.Contracts.Models;
+
+namespace WarehouseManagement.Services.Validators
+{
+    /// <summary>
+    /// Валидатор <<see cref="WarehouseModel"/>
+    /// </summary>
+    internal class WarehouseModelValidator : AbstractValidator<WarehouseModel>
+    {
+        public WarehouseModelValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage(MessageForValidation.DefaultMessage)
+               .NotNull().WithMessage(MessageForValidation.DefaultMessage)
+               .Length(2, 50).WithMessage(MessageForValidation.LengthMessage);
+
+            RuleFor(x => x.Address).NotEmpty().WithMessage(MessageForValidation.DefaultMessage)
+               .NotNull().WithMessage(MessageForValidation.DefaultMessage)
+               .Length(10, 100).WithMessage(MessageForValidation.LengthMessage);
+        }
+    }
+}
